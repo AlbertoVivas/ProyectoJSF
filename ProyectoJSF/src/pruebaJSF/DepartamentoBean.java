59481173b4879466afa59bfd4a6efd90638a17b9
AlbertoVivas <albertovivas@gmail.com>
2015-07-30 21:19:28 +0200

    30/07/15

--- ProyectoJSF/src/pruebaJSF/DepartamentoBean.java
+++ ProyectoJSF/src/pruebaJSF/DepartamentoBean.java
@@ -5,21 +5,26 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.xml.ws.Action;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import servicios.departamentos.DepartmentsServices;
 import tablas_Clases.Departments;
+import tablas_Clases.Employees;
 
 @ManagedBean
-@RequestScoped
+//@RequestScoped//se crea el objeto y se destruye con cada carga de pagina
+@SessionScoped //se crea y se destruye en cada sesion, asi no se llama mil veces y carga mil veces
 public class DepartamentoBean {
 
 	private List<Departments> lista;
-	//private final  Logger log = LogManager.getRootLogger();
+	private List<Employees> listaEmpleados;
+	private int id;
 	
-	@PostConstruct
+	@PostConstruct //para que la informacion que necesita la vista ya este
 	public void recuperarDepartamentos(){
 		List<Departments> listaDepartamentos = null;
 		DepartmentsServices servicioDepartamentos = new DepartmentsServices();
@@ -28,6 +33,11 @@ public class DepartamentoBean {
 		//int i = Integer.parseInt("eer");
 		System.out.println(listaDepartamentos);
 		this.lista = listaDepartamentos;
+	}
+	
+	public void recuperarEmpleadosPorDepartamento(){
+		DepartmentsServices servicioDepartamentos = new DepartmentsServices();
+		listaEmpleados=servicioDepartamentos.obtenerEmpleadosPorDepartamento(id);
 	}
 
 	public List<Departments> getLista() {
@@ -38,13 +48,37 @@ public class DepartamentoBean {
 		this.lista = lista;
 	}
 
-	public DepartamentoBean(List<Departments> lista) {
+	public List<Employees> getListaEmpleados() {
+		return listaEmpleados;
+	}
+
+	public void setListaEmpleados(List<Employees> listaEmpleados) {
+		this.listaEmpleados = listaEmpleados;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public DepartamentoBean(List<Departments> lista, List<Employees> listaEmpleados, int id) {
 		super();
 		this.lista = lista;
+		this.listaEmpleados = listaEmpleados;
+		this.id = id;
 	}
 
 	public DepartamentoBean() {
 		super();
 	}
+	
+
+	
+	
+	
+	
 	
 }
--- ProyectoJSF/src/pruebaJSF/EmpleadoBean.java
+++ ProyectoJSF/src/pruebaJSF/EmpleadoBean.java
@@ -1,11 +1,16 @@
 package pruebaJSF;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.xml.ws.Action;
 
+import hibernateAll.empleadoHibernateDAO.EmpleadoHibernateDAO;
+import interfaces.interfaceRecuperable.Recuperable;
+import servicios.Empleados.EmployeesServices;
 import servicios.departamentos.DepartmentsServices;
 import tablas_Clases.Departments;
 import tablas_Clases.Employees;
@@ -14,17 +19,12 @@ import tablas_Clases.Employees;
 @RequestScoped
 public class EmpleadoBean {
 	
-	private List<Employees> lista;
-
-	public List<Employees> getLista() {
-		return lista;
-	}
-
-	public void setLista(List<Employees> lista) {
-		this.lista = lista;
-	}
 	
-	@PostConstruct
+	private List<Employees> lista;
+	private int id;
+	private Employees empleado;
+	
+	//@Action
 	public void recuperarEmpleados(int id){
 		List<Employees> listaEmpleadosPorDepartamentos = null;
 		DepartmentsServices servicioDepartamentos = new DepartmentsServices();
@@ -35,7 +35,45 @@ public class EmpleadoBean {
 		this.lista = listaEmpleadosPorDepartamentos;
 	}
 	
+	public EmpleadoBean() {
+		super();
+	}
 
-	
+	//@Action
+	public void buscarEmpleado() throws ClassNotFoundException, SQLException{
+		EmployeesServices es = new EmployeesServices();
+		Recuperable objetoDao = new EmpleadoHibernateDAO();
+		es.setRecuperable(objetoDao);
+		this.empleado = (Employees) es.leerEmpleadoServices(this.id);	
+	}
+
+	public EmpleadoBean(List<Employees> lista, int id, Employees empleado) {
+		super();
+		this.lista = lista;
+		this.id = id;
+		this.empleado = empleado;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<Employees> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Employees> lista) {
+		this.lista = lista;
+	}
+	public Employees getEmpleado() {
+		return empleado;
+	}
+
+	public void setEmpleado(Employees empleado) {
+		this.empleado = empleado;
+	}
 
 }
